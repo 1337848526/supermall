3b4cd0d650bfd602645ad1c5c8e282f4cf5f3a83
吴宇哈哈 <75642221+wu_yu_ha@user.noreply.gitee.com>
2020-07-25 10:46:47 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 import router from './router'
-=======
-
->>>>>>> b666c9b78d72e334228cfe4699d29241bd51f379
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-<<<<<<< HEAD
   router
-=======
->>>>>>> b666c9b78d72e334228cfe4699d29241bd51f379
 }).$mount('#app')
